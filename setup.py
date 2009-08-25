9c787370c7a2b78b742813c1dde7116969067116
Uli Fouquet <uli@gnufix.de>
2009-08-25 13:07:31 +0000

    Add a setup.py.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@132 7b4e4b2c-4b64-0410-a84a-e61e0309a823

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1dev'
+
+setup(name='ulif.openoffice',
+      version=version,
+      description="Helpers to bridge different Python envs and OpenOffice.org.",
+      long_description=open("README.txt").read() + "\n\n" +
+                       open(os.path.join("ulif", "openoffice",
+                                         "README.txt")).read() + "\n\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Buildout",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Zope Public License",
+        ],
+      keywords='openoffice pyuno uno openoffice.org',
+      author='Uli Fouquet',
+      author_email='uli at gnufix.de',
+      url='http://pypi.python.org/pypi/ulif.openoffice',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ulif', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zc.buildout',
+      ],
+      extras_require=dict(
+        test = [
+            'zope.testing',
+            ]
+        ),
+      entry_points="""
+      [console_scripts]
+      oooctl = ulif.openoffice.oooctl:main
+      """,
+      )
