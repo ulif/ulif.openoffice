7147c128259ac7c12edd642b5d8862d76b8207c2
Uli Fouquet <uli@gnufix.de>
2009-08-26 23:58:31 +0000

    Add a pyuno daemon.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@163 7b4e4b2c-4b64-0410-a84a-e61e0309a823

---
+++ src/ulif/openoffice/pyunoctl.py
@@ -0,0 +1,68 @@
+##
+## pyunoctl.py
+## Login : <uli@pu.smp.net>
+## Started on  Thu Aug 27 01:50:30 2009 Uli Fouquet
+## $Id$
+## 
+## Copyright (C) 2009 Uli Fouquet
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+import os
+import sys
+from optparse import OptionParser
+from ulif.openoffice.oooctl import daemonize, startstop
+
+PY_BIN = '/usr/bin/python'
+UNO_LIB_DIR = None
+
+def getOptions():
+    usage = "usage: %prog [options] start|stop|restart|status"
+    allowed_args = ['start', 'stop', 'restart', 'status']
+    parser = OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+
+    if len(args) > 1:
+        parser.error("only one argument allowed. Use option '-h' for help.")
+
+    if not os.path.isfile(options.binarypath):
+        parser.error("no such file: %s. Use -b to set the binary path. "
+                     "Use -h to see all options." % options.binarypath)
+        
+    cmd = None
+    if len(args) == 1:
+        cmd = args[0]
+    if cmd not in allowed_args:
+        parser.error("argument must be one of %s. Use option '-h' for help." %
+                     ', '.join(["'%s'" % x for x in allowed_args]))
+    return (cmd, options)
+    
+
+def start(python_binary, uno_lib_dir):
+    print "START PYUNO DAEMON"
+
+def main(argv=sys.argv):
+    if os.name != 'posix':
+        print "This script only works on POSIX compliant machines."
+        sys.exit(-1)
+        
+    (cmd, options) = getOptions()
+
+    # startstop() returns only in case of 'start' or 'restart' cmd...
+    startstop(stderr=options.stderr, stdout=options.stdout,
+              stdin=options.stdin,
+              pidfile=options.pidfile, action=cmd)
+    start(options.binarypath, UNO_LIB_DIR)
+
+    sys.exit(0)
