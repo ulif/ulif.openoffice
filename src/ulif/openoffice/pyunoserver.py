6ef4f36f76c77889c4c706687027acadd1a59e0a
Uli Fouquet <uli@gnufix.de>
2009-08-28 00:21:39 +0000

    Add a server that listens to requests for conversion.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@170 7b4e4b2c-4b64-0410-a84a-e61e0309a823

---
+++ src/ulif/openoffice/pyunoserver.py
@@ -0,0 +1,60 @@
+##
+## pyunoserver.py
+## Login : <uli@pu.smp.net>
+## Started on  Fri Aug 28 02:13:03 2009 Uli Fouquet
+## $Id$
+## 
+## Copyright (C) 2009 Uli Fouquet
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+"""A server that waits for requests for conversion of documents.
+
+Important code fragments are from regular Python documentation.
+"""
+import socket
+import threading
+import SocketServer
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        cur_thread = threading.currentThread()
+        response = "%s: %s" % (cur_thread.getName(), data)
+        self.request.send(response)
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    """An asynchronous TCP server.
+    """
+    pass
+
+def run(host='127.0.0.1', port=2009, python_binary, uno_lib_dir):
+    print "START PYUNO DAEMON"
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 2009
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.setDaemon(True)
+    server_thread.start()
+    print "Server loop running in thread:", server_thread.getName()
+    while 1:
+        pass
