0941390f1449da95008c914cb37f6149e7f82dac
Uli Fouquet <uli@gnufix.de>
2009-09-02 14:53:59 +0000

    Add client components for use by third-party software.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@189 7b4e4b2c-4b64-0410-a84a-e61e0309a823

---
+++ src/ulif/openoffice/client.py
@@ -0,0 +1,29 @@
+##
+## client.py
+## Login : <uli@pu.smp.net>
+## Started on  Wed Sep  2 16:50:35 2009 Uli Fouquet
+## $Id$
+## 
+## Copyright (C) 2009 Uli Fouquet
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+"""Client components for communications with pyunoservers.
+
+This way we can better make sure to have a solid interface for
+third-party software.
+"""
+
+class PyUNOServerClient(object):
+    pass
