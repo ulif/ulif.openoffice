431680d5705b35c3c2d38a34e79d3a68eb096d01
Uli Fouquet <uli@gnufix.de>
2009-09-06 11:18:39 +0000

    Make uno import optional to support loading this package with Plone.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@205 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- src/ulif/openoffice/convert.py
+++ src/ulif/openoffice/convert.py
@@ -37,18 +37,26 @@ from os.path import splitext
 #
 import __builtin__
 _orig__import = __builtin__.__dict__['__import__']
-import uno
-_uno__import = uno.__dict__['_uno_import']
+try:
+    # We make uno import optional to support using this package with
+    # Plone and other frameworks, that blindly import everything.
+    #
+    # Note, that using the server components is only possible, if uno
+    # is available.
+    import uno
+    _uno__import = uno.__dict__['_uno_import']
 
 
-def register_uno_import():
-    __builtin__.__dict__['__import__'] = _uno__import
+    def register_uno_import():
+        __builtin__.__dict__['__import__'] = _uno__import
 
-def unregister_uno_import():
-    __builtin__.__dict__['__import__'] = _orig__import
+    def unregister_uno_import():
+        __builtin__.__dict__['__import__'] = _orig__import
 
-unregister_uno_import()
-
+    unregister_uno_import()
+except ImportError:
+    pass
+    
 def convert_file_to_html(
     url="uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext",
     # A list of internal filter names can be obtained at:
