7c0d9ef79692d593e0b9d396cfbd6dee45fb989b
Uli Fouquet <uli@gnufix.de>
2010-02-10 11:02:41 +0000

    Rewrite of cachemanager: the cachemanager now detects hash collisions and acts appropriately.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@239 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- src/ulif/openoffice/cachemanager.py
+++ src/ulif/openoffice/cachemanager.py
@@ -21,19 +21,154 @@
 ##
 """A manager for storing generated files.
 """
+import filecmp
 import md5 # Deprecated but works also with Python2.4...
 import os
 import shutil
 import sys
+import cPickle as pickle
 
+def internal_suffix(suffix=None):
+    """The suffix used internally in buckets.
+    """
+    if suffix is None:
+        return 'default'
+    return '_' + suffix
+
+class Bucket(object):
+    """A bucket where we store files with same hash sums.
+    """
+    
+    def __init__(self, path):
+        self.path = path
+        self.srcdir = os.path.join(self.path, 'sources')
+        self.resultdir = os.path.join(self.path, 'results')
+        self.create()
+        if self.data is None:
+            self.data = dict(
+                version=0,
+                current_num=0,
+                )
+        self._data = self.data
+
+    def _setInternalData(self, data):
+        data_path = os.path.join(self.path, 'data')
+        pickle.dump(data, open(data_path, 'wb'))
+        return
+
+    def _getInternalData(self):
+        data_path = os.path.join(self.path, 'data')
+        if not os.path.exists(data_path):
+            return None
+        return pickle.load(open(data_path, 'rb'))
+
+    data = property(_getInternalData, _setInternalData)
+    
+    def getCurrentNum(self):
+        """Get current source num.
+        """
+        return self.data['current_num']
+
+    def setCurrentNum(self, num):
+        """Set current source num.
+        """
+        self._data['current_num'] = num
+        self.data = self._data
+        return
+
+    def create(self):
+        """Create the default dirs for this bucket.
+        """
+        for path in (self.path, self.srcdir, self.resultdir):
+            if os.path.exists(path):
+                continue
+            os.makedirs(path)
+        return
+
+    def getSourcePath(self, path):
+        """Get a path to a source file that equals file stored in path.
+
+        Returns a tuple (path, marker) or (None, None) if the source
+        cannot be found.
+        """
+        for filename in os.listdir(self.srcdir):
+            src_path = os.path.join(self.srcdir, filename)
+            filename_parts = filename.split('_', 2)
+            if len(filename_parts) < 2:
+                continue
+            if not filename.startswith('source'):
+                continue
+            if not filecmp.cmp(path, src_path):
+                continue
+            marker = filename_parts[1]
+            return src_path, marker
+        return (None, None)
+
+    def getResultPath(self, path, suffix=None):
+        """Get the cached result for path.
+        """
+        marker = None
+        suffix = internal_suffix(suffix)
+        src_path, marker = self.getSourcePath(path)
+        if src_path is None:
+            return None
+        filename = os.path.basename(src_path)
+        result_filename = 'result_%s_%s' % (marker, suffix)
+        result_path = os.path.join(self.resultdir, result_filename)
+        if os.path.exists(result_path) and os.path.isfile(result_path):
+            return result_path
+        return None
+
+    def storeResult(self, src_path, result_path, suffix=None):
+        """Store file in result_path as result for source in src_path.
+
+        Optionally store this result marked with a certain suffix.
+
+        The result has to be a path to a single file.
+        """
+        suffix = internal_suffix(suffix)
+        local_source, marker = self.getSourcePath(src_path)
+        if local_source is None:
+            # Create new source
+            num = self.getCurrentNum()
+            num += 1
+            self.setCurrentNum(num)
+            marker = str(num)
+            local_source = os.path.join(
+                self.srcdir, 'source_%s' % marker)
+            shutil.copy2(src_path, local_source)
+        # Store result file
+        result_filename = 'result_%s_%s' % (marker, suffix)
+        local_result = os.path.join(self.resultdir, result_filename)
+        shutil.copy2(result_path, local_result)
+        return
+        
 class CacheManager(object):
+    """A cache manager.
 
+    This cache manager caches processed files and their sources. It
+    uses hashes and buckets to find paths of cached files quickly.
+
+    Overall it maps input files on output files. The cache manager is
+    interesting when the computation of an output file is expensive
+    but must be repeated often.
+
+    A sample application is to cache converted office files: as the
+    computation is expensive, we can store the results of conversion
+    in the cache manager and get it any time we want much more
+    quickly. See cachemanager.txt for more infos.
+
+    It also checks for hash collisions: if two input files give the
+    same hash, they will be handled correctly.
+    """
     def __init__(self, cache_dir, level=1):
         self.cache_dir = cache_dir
         self.prepareCacheDir()
         self.level = level # How many dir levels will we create?
 
     def prepareCacheDir(self):
+        """Prepare the cache dir, create dirs, etc.
+        """
         cache_dir = self.cache_dir
 
         if cache_dir is None:
@@ -52,95 +187,46 @@ class CacheManager(object):
             os.mkdir(cache_dir)
             sys.stderr.write('Create cachedir: %s\n' % cache_dir)
         self.cache_dir = cache_dir
-
-    def contains(self, extension=None, data=None, path=None):
-        """Tell whether a document is stored.
-
-        You must pass ``extension`` and either ``data`` or ``path``.
-        """
-        if extension is None:
-            raise ValueError('extension must not be None.')
-        if data is None and path is None:
-            raise ValueError('either path or data must be given.')
-        if not data is None and not path is None:
-            raise ValueError('only one of data or path might be given')
-        md5_digest = self.getMD5Digest(data=data, path=path)
-        dir = self.getCacheDir(extension=extension, md5_digest=md5_digest)
-        if not os.path.exists(dir):
-            return False
-        return True
-
-    def getMD5Digest(self, data=None, path=None):
-        """Get the MD5 sum of a file.
-
-        You can pass the file contents (``data``) **or** the ``path``.
-        """
-        if data is not None:
-            return md5.new(data).hexdigest()
-        return md5.new(open(path, 'r').read()).hexdigest()
-
-    def getCacheDir(self, extension, md5_digest):
-        """Get the cache dir where a document with given parameters would be
-           stored.
-
-        This does not guarantee, that the path really exists.
-        """
-        parent_dir = [md5_digest[x*2:x*2+2]
-                      for x in range((self.level+1))][:-1]
-        parent_dir.append(md5_digest)
-        extension = extension.lower()
-        parent_cache_dir = os.path.join(*parent_dir)
-        parent_cache_dir = os.path.join(
-            self.cache_dir, parent_cache_dir, extension)
-        return parent_cache_dir
-
-    def registerDoc(self, source_path, to_cache):
-        """Register the document at path ``to_cache`` generated from
-           ``source_path``.
-
-           Both paths should refer to files, not directories.
-        """
-        md5_digest = self.getMD5Digest(path=source_path)
-        ext = os.path.splitext(to_cache)[1][1:].lower()
-        dir = self.getCacheDir(ext, md5_digest)
-
-        if ext in ['pdf',]:
-            # Copy only the result doc...
-            dst = os.path.join(dir, os.path.basename(to_cache))
-            os.makedirs(dir)
-            shutil.copy2(to_cache, dst)
-            self.markCacheDir(dir, to_cache)
-            return
-        # Copy all files in result dir...
-        dir_to_cache = os.path.dirname(to_cache)
-        os.makedirs(dir)
-        for filename in os.listdir(dir_to_cache):
-            fullpath = os.path.join(dir_to_cache, filename)
-            if not os.path.isfile(fullpath):
-                # Ignore subdirs.
-                continue
-            if filename == os.path.basename(source_path):
-                # Ignore source document.
-                continue
-            dst = os.path.join(dir, filename)
-            shutil.copy2(fullpath, dst)
-            self.markCacheDir(dir, to_cache)
         return
 
-    def getCachedDocPath(self, source_path, ext):
-        """Get a cached docs path or ``None``.
+    def getBucketFromPath(self, path):
+        """Get a bucket in which the source given by path would be stored.
         """
-        md5_digest = self.getMD5Digest(path=source_path)
-        ext = ext.lower()
-        dir = self.getCacheDir(ext, md5_digest)
-        if not os.path.isdir(dir):
-            return None
-        cached_file = open(os.path.join(dir, 'MAINDOC'), 'r').read()
-        return os.path.join(dir, cached_file)
+        md5_digest = self.getHash(path)
+        return self.getBucketFromHash(md5_digest)
 
-    def markCacheDir(self, cache_dir, doc_to_cache):
-        """Create an entry in cache dir to remember main file.
+    def getBucketFromHash(self, hash_digest):
+        """Get a bucket in which a source with 'hash_digest' would be stored.
         """
-        mainfile_name = os.path.basename(doc_to_cache)
-        marker_file = os.path.join(cache_dir, 'MAINDOC')
-        open(marker_file, 'w').write(mainfile_name)
+        dirs = [hash_digest[x*2:x*2+2]
+                for x in range((self.level+1))][:-1]
+        dirs.append(hash_digest)
+        bucket_path = os.path.join(self.cache_dir, *dirs)
+        return Bucket(bucket_path)
+
+    def getCachedFile(self, path, suffix=None):
+        """Check, whether the file in ``path`` is already cached.
+
+        Returns the path of cached file or None.
+        """
+        bucket = self.getBucketFromPath(path)
+        return bucket.getResultPath(path, suffix=suffix)
+
+    def registerDoc(self, source_path, to_cache, suffix=None):
+        """Store to_cache in bucket.
+        """
+        bucket = self.getBucketFromPath(source_path)
+        bucket.storeResult(source_path, to_cache, suffix)
+        return
+
+    def getHash(self, path=None):
+        """Get the hash of a file.
+
+        Currently we compute the MD5 digest.
+        """
+        return md5.new(open(path, 'r').read()).hexdigest()
+
+    def contains(self, path, suffix=None):
+        """Check, whether the file in ``path`` is already cached.
+        """
+        return self.getCachedFile(path, suffix=suffix) is not None
