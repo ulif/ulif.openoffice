687830dddd944697188a495956eb3fa5d99dcbf1
Uli Fouquet <uli@gnufix.de>
2010-05-15 16:32:57 +0000

    Enable autodoc.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@295 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,9 @@ import pkg_resources
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig',
+              'sphinx.ext.autodoc']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
