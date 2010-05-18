a8c2385cada4b32bb1b1e8b2f10d08673f31d10a
Uli Fouquet <uli@gnufix.de>
2010-05-18 15:57:49 +0000

    Be explicit about restserver.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/trunk@301 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- src/ulif/openoffice/restserver.py
+++ src/ulif/openoffice/restserver.py
@@ -20,6 +20,8 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##
 """A conversion server using REST.
+
+   This server is not in a usable state yet.
 """
 import cgi
 import os
