7402d24dbc2f87d0b82519b4214ac35756e73468
Uli Fouquet <uli@gnufix.de>
2011-04-29 13:49:34 +0000

    Disable most tests that were based on dummy resources in restserver.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/branches/ulif-webservice@770 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- src/ulif/openoffice/tests/test_rest_cherrypy.py
+++ src/ulif/openoffice/tests/test_rest_cherrypy.py
@@ -19,118 +19,37 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##
-import cherrypy
-import logging
 import os
 import shutil
-import tempfile
-import time
 try:
     import unittest2 as unittest
 except:
     import unittest
-import ulif.openoffice
-from ulif.openoffice.oooctl import check_port
-from StringIO import StringIO
-from restclient import RestClient, Resource 
-from webtest import TestApp 
-from ulif.openoffice.restserver2 import Root, conf
-from ulif.openoffice.tests.test_ulifopenoffice import setUp
 
-wsgi_app = None
+from ulif.openoffice.testing import (
+    TestRESTfulWSGISetup, TestRESTfulFunctionalSetup
+    )
 
-class TestRESTfulWSGILayer(unittest.TestCase):
+class TestRESTful(TestRESTfulWSGISetup):
 
-    def setUp(self):
-        self.workdir = tempfile.mkdtemp()
-        self.access_log = os.path.join(self.workdir, 'access.log')
-        self.error_log = os.path.join(self.workdir, 'error.log')
-        
-        # configure cherrypy to be quiet ;)
-        cherrypy.config.update({ "environment": "embedded" })
-
-        cherrypy.config.update(
-            {'log.access_file': self.access_log,
-             'log.error_file': self.error_log,
-             'log.screen': False,
-             }
-            )
-        
-        # Keep pyflakes happy
-        #self.output = StringIO()
-
-        #self.resource = Resource('http://localhost:8080')
-
-        self.wsgi_app = cherrypy.Application(Root(), '/', config=conf)
-        self.app = TestApp(self.wsgi_app)
-
-    def tearDown(self):
-        shutil.rmtree(self.workdir)
-        del self.app
-        del self.wsgi_app
-        return
-
-class TestRESTfulFunctionalSetup(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Set clean HOME environment as OOO.org might scan it...
-        cls._marker = object()
-        cls.old_home = os.environ.get('HOME', cls._marker)
-        cls.homedir = tempfile.mkdtemp()
-        os.environ['HOME'] = cls.homedir
-
-        # Don't start oooctl if it runs already...
-        cls.ooo_running = False
-        if check_port('localhost', 2002):
-            cls.ooo_running = True
-            return
-
-        # Start oooctl...
-        path = os.path.dirname(ulif.openoffice.__file__)
-        cls.oooctl_path = os.path.abspath(os.path.join(
-                path, '..', '..', '..', 'bin', 'oooctl'))
-        os.system(cls.oooctl_path + ' --stdout=/tmp/oooctl.log start')
-        time.sleep(3)
-        return
-
-    @classmethod
-    def tearDownClass(cls):
-        # Only shut down oooctl if it were not running already...
-        if cls.ooo_running is not True:
-            os.system(cls.oooctl_path + ' stop')
-
-        # Clean up dirs...
-        shutil.rmtree(cls.homedir)
-
-        # Set HOME to old state...
-        if cls.old_home is cls._marker:
-            del os.environ['HOME']
-        else:
-            os.environ['HOME'] = cls.old_home
-
-        return
-
-
-class TestRESTful(TestRESTfulWSGILayer):
-
-    def test_foo(self):
+    def NOtest_foo(self):
         response = self.app.get('/index')
         self.assertTrue(response.body.startswith('<html>'))
         pass
 
-    def test_status_200(self):
+    def NOtest_status_200(self):
         response = self.app.get('/sidewinder')
         self.assertEqual(response.status, '200 OK')
         return
 
-    def test_header_item(self):
+    def NOtest_header_item(self):
         response = self.app.get('/sidewinder')
         headers = response.headers
         self.assertTrue('content-type' in headers)
         assert headers.get('content-type', None) == 'text/html;charset=utf-8'
         #self.assertEqual(headers.get('content-type', None), 'text/html')
 
-    def test_POST(self):
+    def NOtest_POST(self):
         response = self.app.post(
             '/pdf',
             params={'var1':'value1'}, headers=None, extra_environ=None,
@@ -152,7 +71,7 @@ class TestRESTful(TestRESTfulWSGILayer):
         #self.assertEqual(1, 1)
 
 
-class TestRESTfulFunctional(TestRESTfulWSGILayer, TestRESTfulFunctionalSetup):
+class TestRESTfulFunctional(TestRESTfulWSGISetup, TestRESTfulFunctionalSetup):
     def setUp(self):
         super(TestRESTfulFunctional, self).setUp()
         self.resultdir = None
@@ -179,7 +98,7 @@ class TestRESTfulFunctional(TestRESTfulWSGILayer, TestRESTfulFunctionalSetup):
         assert basename == u'mytest.html'
         return
 
-    def test_POST(self):
+    def NOtest_POST(self):
         response = self.app.post(
             '/docs',
             params={'doc_id':'12'},
