444fb0fe077fa378df85209f94f57fdda436522d
Uli Fouquet <uli@gnufix.de>
2011-05-01 01:12:00 +0000

    Add resources.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/branches/ulif-webservice@778 7b4e4b2c-4b64-0410-a84a-e61e0309a823

---
+++ src/ulif/openoffice/tests/test_resources.py
@@ -0,0 +1,82 @@
+##
+## test_resources.py
+## Login : <uli@pu.smp.net>
+## Started on  Fri Apr 29 15:15:27 2011 Uli Fouquet
+## $Id$
+## 
+## Copyright (C) 2011 Uli Fouquet
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+import os
+import shutil
+import tempfile
+try:
+    import unittest2 as unittest
+except:
+    import unittest
+from ulif.openoffice.cachemanager import CacheManager
+from ulif.openoffice.resource import Resource, create_resource, get_resource
+from ulif.openoffice.testing import (
+    TestRESTfulWSGISetup, TestOOServerSetup
+    )
+
+class TestResources(TestOOServerSetup):
+
+    def setUp(self):
+        self.workdir = tempfile.mkdtemp()
+        self.cachedir = tempfile.mkdtemp()
+        self.samplefile = os.path.join(self.workdir, 'sample.txt')
+        open(self.samplefile, 'wb').write('Some sample text\n')
+        return
+
+    def tearDown(self):
+        shutil.rmtree(self.workdir)
+        shutil.rmtree(self.cachedir)
+        return
+
+    def remove_resultfiles(self, path):
+        dir_to_remove = path
+        if path is None:
+            return
+        if os.path.isfile(path):
+            dir_to_remove = os.path.dirname(path)
+        if os.path.isdir(dir_to_remove):
+            shutil.rmtree(dir_to_remove)
+        return
+
+    def test_create_none(self):
+        # If we pass no file, we will get no result
+        status, hash, path = create_resource(None)
+        assert status != 0
+        assert hash is None
+        assert path is None
+        
+    def test_create_invalid(self):
+        # If we pass an unprocessable doc, we will get no result
+        invalid_stuff = os.path.join(self.workdir, 'trash.jpg')
+        open(invalid_stuff, 'wb').write('nonsense')
+        status, hash, path = create_resource(invalid_stuff)
+        self.remove_resultfiles(path)
+        assert status != 0
+        assert hash is None
+        assert path is None
+        
+
+    def test_create_simple_got_file(self):
+        # If we give no further parameters, we will get a document...
+        status, hash, path = create_resource(self.samplefile)
+        assert os.path.isfile(path)
+        self.remove_resultfiles(path)
+        return
