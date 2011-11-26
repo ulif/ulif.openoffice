ca26c6aeb9d06ebda0c5c5f694ab44f7c08744f1
Uli Fouquet <uli@gnufix.de>
2011-11-26 21:57:15 +0000

    Add local pytest plugin.
    git-svn-id: https://svn.gnufix.de/repos/main/ulif.openoffice/branches/ulif-webservice@1011 7b4e4b2c-4b64-0410-a84a-e61e0309a823

--- src/ulif/openoffice/tests/conftest.py
+++ src/ulif/openoffice/tests/conftest.py
@@ -6,3 +6,25 @@ def pytest_addoption(parser):
 def pytest_runtest_setup(item):
     if 'slow' in item.keywords and not item.config.getvalue("runslow"):
         pytest.skip("need --runslow option to run")
+
+class MyPlugin:
+    def pytest_pycollect_makeitem(collector, name, obj):
+        unittest = sys.modules.get('unittest')
+        if unittest is None:
+            return # nobody can have derived unittest.TestCase
+        try:
+            issuite = isinstance(obj, unittest.TestSuite)
+        except KeyboardInterrupt:
+            raise
+        except Exception:
+            pass
+        else:
+            if issuite:
+                return UnitTestSuite(name, parent=collector)
+        return None
+
+def pytest_collect_file(path, parent):
+    return
+
+class UnitTestSuite(pytest.Class):
+    pass
