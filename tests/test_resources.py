2626fa1e3ef748b533005a760026c6845994692d
ulif <uli@gnufix.fake>
2013-06-05 09:48:26 +0200

    PEP8.

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,20 +1,17 @@
 ##
 ## test_resources.py
-## Login : <uli@pu.smp.net>
-## Started on  Fri Apr 29 15:15:27 2011 Uli Fouquet
-## $Id$
-## 
-## Copyright (C) 2011 Uli Fouquet
+##
+## Copyright (C) 2011, 2013 Uli Fouquet
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -31,6 +28,7 @@ from ulif.openoffice.resource import Resource, create_resource, get_resource
 from ulif.openoffice.testing import (
     TestRESTfulWSGISetup, TestOOServerSetup
     )
+
 
 class TestResources(TestOOServerSetup):
 
