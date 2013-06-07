2356709c4ce3cb388b1264400250391812abd4d9
ulif <uli@gnufix.fake>
2013-06-07 13:58:38 +0200

    Test paste.deploy compatibility.

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -1,10 +1,21 @@
 # tests for wsgi module
+import os
+import shutil
+import tempfile
 import unittest
 from webob import Request
 from ulif.openoffice.wsgi import RESTfulDocConverter
 
 
 class DocConverterFunctionalTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.workdir = tempfile.mkdtemp()
+        self.inputdir = os.path.join(os.path.dirname(__file__), 'input')
+        self.paste_conf1 = os.path.join(self.inputdir, 'sample1.ini')
+
+    def tearDown(self):
+        shutil.rmtree(self.workdir)
 
     def test_restful_doc_converter(self):
         # we can create a RESTful sample app
@@ -22,4 +33,12 @@ class DocConverterFunctionalTestCase(unittest.TestCase):
         req = Request.blank('http://localhost/docs')
         resp = app(req)
         self.assertEqual(resp.status, '200 OK')
+        return
+
+    def test_paste_deploy_loader(self):
+        # we can find the docconverter via paste.deploy plugin
+        from paste.deploy import loadapp
+        app = loadapp(
+            'config:%s' % self.paste_conf1)
+        self.assertTrue(isinstance(app, RESTfulDocConverter))
         return
