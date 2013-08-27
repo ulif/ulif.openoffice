345593af7aff2267b704f1fbf663355c6d9e7167
ulif <uli@gnufix.fake>
2013-08-27 02:26:57 +0200

    Ignore coverage for some not very interesting pieces.

--- src/ulif/openoffice/helpers.py
+++ src/ulif/openoffice/helpers.py
@@ -101,21 +101,21 @@ def copytree(src, dst, symlinks=False, ignore=None):
             else:
                 shutil.copy2(srcname, dstname)
             # XXX What about devices, sockets etc.?
-        except (IOError, os.error) as why:
+        except (IOError, os.error) as why:  # pragma: no cover
             errors.append((srcname, dstname, str(why)))
         # catch the Error from the recursive copytree so that we can
         # continue with other files
-        except (Error) as err:
+        except (Error) as err:              # pragma: no cover
             errors.extend(err.args[0])
     try:
         shutil.copystat(src, dst)
-    except WindowsError:
+    except WindowsError:                    # pragma: no cover
         # can't copy file access times on Windows
         pass
-    except (OSError) as why:
+    except (OSError) as why:                # pragma: no cover
         errors.extend((src, dst, str(why)))
     if errors:
-        raise shutil.Error(errors)
+        raise shutil.Error(errors)          # pragma: no cover
 
 
 def copy_to_secure_location(src):
