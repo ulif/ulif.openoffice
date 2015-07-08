af7f0182205f1d921ed3e5624ed25029f6b3214f
ulif <uli@gnufix.de>
2015-07-08 21:17:36 +0200

    Update copyright year.

--- src/ulif/openoffice/processor.py
+++ src/ulif/openoffice/processor.py
@@ -1,7 +1,7 @@
 ##
 ## processor.py
 ##
-## Copyright (C) 2011, 2013 Uli Fouquet
+## Copyright (C) 2011, 2013, 2015 Uli Fouquet
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or
