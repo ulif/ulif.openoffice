75844937babe6b03041569e1d651528f48ff3e3e
ulif <uli@gnufix.de>
2015-08-07 18:27:39 +0200

    pep8.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-##
-## test_helpers.py
-##
-## Copyright (C) 2011, 2013, 2015 Uli Fouquet
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##
+#
+# test_helpers.py
+#
+# Copyright (C) 2011, 2013, 2015 Uli Fouquet
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 from __future__ import unicode_literals
 import os
 import shutil
@@ -585,7 +585,6 @@ class TestHelpers(unittest.TestCase):
         assert base64url_encode("foo") == "Zm9v"
         assert base64url_encode(b"foo") == "Zm9v"
         assert base64url_encode(u"foo") == "Zm9v"
-        #assert not isinstance(base64url_encode(u"foo"), unicode)
 
     def test_base64url_decode(self):
         exp1 = chr(251) + chr(239)
