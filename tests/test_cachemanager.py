af66e46bd2c0ca137e8aa4df2e1badb7cd15c604
ulif <uli@gnufix.de>
2015-08-10 18:14:14 +0200

    py3 compat: fix cachemanager test.
    Markers are not bytes but text.

--- tests/test_cachemanager.py
+++ tests/test_cachemanager.py
@@ -387,16 +387,16 @@ class TestCacheManager(CachingComponentsTestCase):
         marker2 = cm.register_doc(
             self.src_path1, self.result_path1)
         marker3 = cm.register_doc(
-            self.src_path1, self.result_path2, repr_key=b'foo')
+            self.src_path1, self.result_path2, repr_key='foo')
         marker4 = cm.register_doc(
-            self.src_path2, self.result_path2, repr_key=b'foo')
+            self.src_path2, self.result_path2, repr_key='foo')
         marker5 = cm.register_doc(
-            self.src_path2, self.result_path2, repr_key=StringIO(b'bar'))
-        self.assertEqual(marker1, b'737b337e605199de28b3b64c674f9422_1_1')
-        self.assertEqual(marker2, b'737b337e605199de28b3b64c674f9422_1_1')
-        self.assertEqual(marker3, b'737b337e605199de28b3b64c674f9422_1_2')
-        self.assertEqual(marker4, b'd5aa51d7fb180729089d2de904f7dffe_1_1')
-        self.assertEqual(marker5, b'd5aa51d7fb180729089d2de904f7dffe_1_2')
+            self.src_path2, self.result_path2, repr_key=StringIO('bar'))
+        self.assertEqual(marker1, '737b337e605199de28b3b64c674f9422_1_1')
+        self.assertEqual(marker2, '737b337e605199de28b3b64c674f9422_1_1')
+        self.assertEqual(marker3, '737b337e605199de28b3b64c674f9422_1_2')
+        self.assertEqual(marker4, 'd5aa51d7fb180729089d2de904f7dffe_1_1')
+        self.assertEqual(marker5, 'd5aa51d7fb180729089d2de904f7dffe_1_2')
         return
 
     def test_get_hash(self):
