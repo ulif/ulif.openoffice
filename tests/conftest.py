ccbd5961cb50f8f84ab56d5cadb3a5eb0278afb0
ulif <uli@gnufix.de>
2015-08-13 19:29:35 +0200

    Fix fixture :-)
    Take care that virtualenv is removed from os.environ["PATH"] and
    restored after tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,32 @@ import os
 import pytest
 
 @pytest.fixture(scope="function")
-def reset_sys_path(request):
+def envpath_no_venv(request):
+    """Strip virtualenv path from system environment $PATH.
+
+    For the test remove virtualenv path from $PATH.
+
+    We use this fixture here to ensure that virtualenvs can be used in
+    tests but do not interfere with `unoconv` path and needed libs.
+
+    In other words: with this fixture we can run tests in Python
+    versions, that normally do not support `uno` and other packages
+    needed by `unoconv`.
+    """
     _path = os.environ.get('PATH', None)
+    if not _path:
+        return
+    v_env_path = os.environ.get('VIRTUAL_ENV', None)
+    if not v_env_path or (v_env_path not in _path):
+        return
+    new_path = ":".join([
+        x for x in _path.split(":")
+        if v_env_path not in x]
+        )
+    os.environ["PATH"] = new_path
 
     def teardown():
         if _path is not None:
-            os.environ.set('PATH', _path)
+            os.environ['PATH'] = _path
 
     request.addfinalizer(teardown)
