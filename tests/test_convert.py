5c7733b9fcd4350c75fe25619393106c27529bf8
ulif <uli@gnufix.de>
2015-08-18 00:06:35 +0200

    py.testify another test.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -18,18 +18,6 @@ class ConvertTests(TestOOServerSetup):
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
 
-    def test_simple_conversion_to_html(self):
-        # we can convert a simple text file to html
-        path = os.path.join(self.tmpdir, 'sample.txt')
-        open(path, 'w').write('Hi there!\n')
-        status, result_dir = convert(out_format='html', path=path)
-        self.assertEqual(0, status)
-        self.assertEqual(['sample.html'], os.listdir(result_dir))
-        result_doc = open(os.path.join(result_dir, 'sample.html'), 'rb').read()
-        self.assertEqual(result_doc[:10], b'<!DOCTYPE ')
-        shutil.rmtree(result_dir)  # clean up
-        return
-
     def test_convert_outdir(self):
         # the outdir parameter is respected
         path = os.path.join(self.tmpdir, 'sample.txt')
@@ -93,5 +81,17 @@ class TestConvert(object):
         assert os.listdir(result_dir) == ['sample.pdf']
         result_doc = open(os.path.join(result_dir, 'sample.pdf'), 'rb').read()
         assert result_doc.startswith(b'%PDF-1.4\n%')
+        shutil.rmtree(result_dir)  # clean up
+        return
+
+    def test_simple_conversion_to_html(self, run_lo_server, tmpdir):
+        # we can convert a simple text file to html
+        path = tmpdir.join('sample.txt')
+        path.write('Hi there!\n')
+        status, result_dir = convert(out_format='html', path=str(path))
+        assert status == 0
+        assert os.listdir(result_dir) == ['sample.html']
+        result_doc = open(os.path.join(result_dir, 'sample.html'), 'rb').read()
+        assert result_doc.startswith(b'<!DOCTYPE ')
         shutil.rmtree(result_dir)  # clean up
         return
