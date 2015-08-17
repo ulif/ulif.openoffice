c773c675a18c3b8896bf48524608177b5b2b6a47
ulif <uli@gnufix.de>
2015-08-17 23:58:57 +0200

    Remove obsolete test.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -18,18 +18,6 @@ class ConvertTests(TestOOServerSetup):
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
 
-    def test_simple_conversion_to_pdf(self):
-        # we can convert a simple text file to pdf
-        path = os.path.join(self.tmpdir, 'sample.txt')
-        open(path, 'w').write('Hi there!\n')
-        status, result_dir = convert(out_format='pdf', path=path)
-        self.assertEqual(0, status)
-        self.assertEqual(['sample.pdf'], os.listdir(result_dir))
-        result_doc = open(os.path.join(result_dir, 'sample.pdf'), 'rb').read()
-        self.assertEqual(result_doc[:10], b'%PDF-1.4\n%')
-        shutil.rmtree(result_dir)  # clean up
-        return
-
     def test_simple_conversion_to_html(self):
         # we can convert a simple text file to html
         path = os.path.join(self.tmpdir, 'sample.txt')
