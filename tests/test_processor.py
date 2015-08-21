6af230c1478721de1a9e364891329a6b570e64a6
ulif <uli@gnufix.de>
2015-08-21 10:08:44 +0200

    Fix test: write bytes to binary files.

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -544,11 +544,11 @@ class TestZipProcessor(unittest.TestCase):
     def test_simple(self):
         sample_path = os.path.join(self.workdir, 'sample1.txt')
         with open(sample_path, 'wb') as fd:
-            fd.write('Hi there!')
+            fd.write(b'Hi there!')
         with open(
                 os.path.join(self.workdir, 'sample2.txt'),
                 'wb') as fd:
-            fd.write('Hello again')
+            fd.write(b'Hello again')
         proc = ZipProcessor()
         self.result_path, metadata = proc.process(
             sample_path, {'error': False})
