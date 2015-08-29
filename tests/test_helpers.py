f6053fe7c32750337454385a20240442860a9e3b
ulif <uli@gnufix.de>
2015-08-29 02:15:27 +0200

    Remove obsolete doctest.
    Was modified and moved into new helpers test case.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -266,64 +266,6 @@ class TestHelpers(unittest.TestCase):
             shutil.rmtree(path)
         return
 
-    def test_extract_css(self):
-        """
-        >> from ulif.openoffice.helpers import extract_css
-        >> html, css = extract_css('''
-        ... <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        ... "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        ... <html xmlns="http://www.w3.org/1999/xhtml">
-        ... <head>
-        ... <meta name="generator" content="HTML Tidy for Linux/x86" />
-        ... <meta http-equiv="CONTENT-TYPE" content="text/html;
-        ...       charset=utf-8" />
-        ...          <title>
-        ...          </title>
-        ...          <meta name="GENERATOR"
-        ...                content="OpenOffice.org 2.4 (Linux)" />
-        ...          <style type="text/css">
-        ...           /* <![CDATA[ */
-        ...            @page { size: 21cm 29.7cm; margin: 2cm }
-        ...            p { margin-bottom: 0.21cm }
-        ...            span.c2 {font-family: DejaVu Sans Mono, sans-serif}
-        ...            p.c1 {margin-bottom: 0cm}
-        ...           /* ]]> */
-        ...          </style>
-        ...         </head>
-        ...         <body lang="de-DE" dir="ltr" xml:lang="de-DE">
-        ...         </body>
-        ...        </html>
-        ... ''', 'sample.html')
-
-      The returned css part contains all styles from input:
-
-        >> print css
-        @page { size: 21cm 29.7cm; margin: 2cm }
-        p { margin-bottom: 0.21cm }
-        span.c2 {font-family: DejaVu Sans Mono, sans-serif}
-        p.c1 {margin-bottom: 0cm}
-
-      The returned HTML part has the styles replaced with a link:
-
-        >> print html # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-         <html xmlns="http://www.w3.org/1999/xhtml">
-          <head>
-           <meta name="generator" content="HTML Tidy for Linux/x86" />
-           <meta http-equiv="CONTENT-TYPE" content="text/html;
-               charset=utf-8" />
-           <title>
-           </title>
-          <meta name="GENERATOR" content="OpenOffice.org 2.4 (Linux)" />
-          <link rel="stylesheet" type="text/css" href="sample.css" />
-         </head>
-         <body lang="de-DE" dir="ltr" xml:lang="de-DE">
-         </body>
-        </html>
-
-        """
-
     def test_extract_css_trash(self):
         # Also trashy docs can be handled
         result, css = extract_css("", 'sample.html')
