03845355b5f8aaeae4a3dc5586786eb92b4ffc35
ulif <uli@gnufix.de>
2015-09-03 16:37:56 +0200

    Add docs for tests.

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -5,15 +5,18 @@ class TestEnvPathWithoutVirtualEnvs(object):
     # tests for envpath_wo_virtualenvs.
 
     def test_no_PATH_set(self, monkeypatch):
+        # we require $PATH to be set.
         monkeypatch.delenv("PATH", raising=False)
         assert envpath_wo_virtualenvs() is None
 
     def test_no_VIRTUAL_ENV_set(self, monkeypatch):
+        # we require $VIRTUAL_ENV to be set.
         monkeypatch.setenv("PATH", "/tmp:/foo")
         monkeypatch.delenv("VIRTUAL_ENV", raising=False)
         assert envpath_wo_virtualenvs() == "/tmp:/foo"
 
     def test_single_virt_env_set(self, monkeypatch):
+        # with a single virtual env set we get a path list without it.
         monkeypatch.setenv("PATH", "/myvenv1/bin:/foo")
         monkeypatch.setenv("VIRTUAL_ENV", "/myvenv1")
         assert envpath_wo_virtualenvs() == "/foo"
