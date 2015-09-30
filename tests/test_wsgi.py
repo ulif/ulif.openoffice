49a7290450e7ae6642c6e1096b67957eee5fd6a5
ulif <uli@gnufix.de>
2015-09-30 16:12:04 +0200

    Remove obsolete test.

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -125,14 +125,6 @@ class DocConverterFunctionalTestCase(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.workdir)
 
-    def test_restful_doc_converter(self):
-        # we can create a RESTful sample app
-        app = RESTfulDocConverter()
-        req = Request.blank('http://localhost/test.html')
-        resp = app(req)
-        self.assertEqual(resp.status, '404 Not Found')
-        return
-
     def test_restful_doc_converter_simple_get(self):
         # RESTful sample app handles simple GET
         app = RESTfulDocConverter()
