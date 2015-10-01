1ebb225dbf5a42774ff29e6b9001257a9b3b5c54
ulif <uli@gnufix.de>
2015-10-01 13:05:56 +0200

    Remove obsolete test.

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -247,39 +247,6 @@ class DocConverterFunctionalTestCase(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.workdir)
 
-    def test_create_out_fmt_respected(self):
-        # a single out_fmt option will result in appropriate output format
-        # (the normal option name would be 'oocp.out_fmt')
-        app = RESTfulDocConverter(cache_dir=self.cachedir)
-        myform = dict(
-            doc=('sample.txt', 'Hi there!'),
-            CREATE='Send', out_fmt='pdf',
-            )
-        myform = {
-            'doc': ('sample.txt', 'Hi there!'),
-            'CREATE': 'Send',
-            'out_fmt': 'pdf',
-            }
-        req = Request.blank(
-            'http://localhost/docs',
-            POST=myform
-            )
-        resp = app(req)
-        # we get a location header
-        location = resp.headers['Location']
-        self.assertEqual(
-            location,
-            'http://localhost:80/docs/396199333edbf40ad43e62a1c1397793_1_1')
-        self.assertEqual(resp.status, '201 Created')
-        self.assertEqual(resp.headers['Content-Type'], 'application/zip')
-        content_file = os.path.join(self.workdir, 'myresult.zip')
-        with open(content_file, 'wb') as fd:
-            fd.write(resp.body)
-        self.assertTrue(zipfile.is_zipfile(content_file))
-        myzipfile = zipfile.ZipFile(content_file, 'r')
-        self.assertTrue('sample.pdf' in myzipfile.namelist())
-        return
-
     def test_show_yet_uncached_doc(self):
         # a yet uncached doc results in 404
         app = RESTfulDocConverter(cache_dir=self.cachedir)
