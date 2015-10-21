9b400c52944152210e3a5bfa601191c5b94b0b03
ulif <uli@gnufix.de>
2015-10-21 14:11:45 +0200

    Fix tests to really check exit codes.

--- tests/test_oooctl.py
+++ tests/test_oooctl.py
@@ -14,12 +14,12 @@ class TestOOOCtl(object):
     def test_get_options_no_argv(self):
         with pytest.raises(SystemExit) as why:
             get_options(argv=[])
-            why.code == 2
+        assert why.value.code == 2
 
     def test_get_options_invalid_command(self):
         with pytest.raises(SystemExit) as why:
             get_options(argv=['fakeoooctl', 'maybestart'])
-            why.code == 2
+        assert why.value.code == 2
 
     def test_get_options_too_many_args(self):
         with pytest.raises(SystemExit) as why:
@@ -30,4 +30,4 @@ class TestOOOCtl(object):
         # we should not pass an invalid path to executable
         with pytest.raises(SystemExit) as why:
             get_options(argv=['fakeoooctl', '-b', 'invalid-path', 'start'])
-            assert why.code == 2
+        assert why.value.code == 2
