56cbeef7af1110019f94ec0636f1b51d5f6c0420
ulif <uli@gnufix.de>
2016-06-14 03:49:40 +0200

    Fix another encoding problem.

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -600,8 +600,8 @@ class TestCSSCleanerProcessor(object):
         resultpath, metadata = proc.process(
             str(workdir / "src" / "sample.html"), {'error': False})
         resultdir = os.path.dirname(resultpath)
-        result_css = open(
-            os.path.join(resultdir, 'sample.css'), 'r').read()
+        result_css = codecs.open(
+            os.path.join(resultdir, 'sample.css'), 'r', 'utf-8').read()
         assert 'p{margin-bottom:.21cm}span.c2' in result_css
 
     def test_cleaner_css_non_minified(self, workdir, samples_dir):
